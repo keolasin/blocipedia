1d9c47db119fd781e90def84ecc95a8404669d93
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-07-03 09:52:02 -0700

    add basic server/route config

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,19 @@
+// include request module and server.js
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000";
+
+// static route
+describe("routes : static", () => {
+
+  // GET root path "/" spec
+  describe("GET /", () => {
+    // server should return okay status code if it's running
+    it("should return status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
+// require modules (express);
 const express = require("express");
+
+// require config files
+const appConfig = require("./config/main-config.js");
+const routeConfig = require("./config/route-config.js");
+
+// create express app
 const app = express();
+
+// use routeConfig
+routeConfig.init(app);
+appConfig.init();
 
 module.exports = app;
