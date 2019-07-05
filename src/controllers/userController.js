a30eba2972db6f206c0a663a08639d70630e9f90
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-07-05 10:59:19 -0700

    add controllers, routes for Users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,51 @@
 module.exports = {
+  create(req, res, next){
+    // pull values from our request body and add them to a newUser object
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      password_conf: req.body.password_conf
+    };
+
+    // use helper query createUser() from userQueries, pass in newUser and callback
+    userQueries.createUser(newUser, (err, user) => {
+      if(err){ // check for and flash error if there is one
+        req.flash("error", err);
+        res.redirect("/users/sign_up");
+      } else { // user created successfully
+        // authorize user via passport local strategy
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've successfully signed in!");
+          res.redirect("/");
+        })
+      }
+    });
+  },
+
   signUp(req, res, next){
     res.render("users/sign_up");
+  },
+
+  signIn(req, res, next){
+    // auth via passport local strategy
+    passport.authenticate("local")(req, res, () => {
+      if(!req.user){
+        req.flash("notice", "Sign in failed, please try again");
+        res.redirect("/users/sign_in");
+      } else {
+        req.flash("notice", "You've successfully signed in!");
+        res.redirect("/");
+      }
+    });
+  },
+
+  signInForm(req, res, next){
+    res.render("users/sign_in")
+  },
+
+  signOut(req, res, next){
+    req.logout();
+    req.flash("notice", "You've successfully signed out!");
+    res.redirect("/");
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -9,9 +9,13 @@ const userController = require("../controllers/userController");
 // define routes
   // GET
 router.get("/users/sign_up", userController.signUp);
+router.get("/users/sign_in", userController.signInForm);
+router.get("/users/sign_out", userController.signOut);
 
   // POST
+router.post("/users", validation.validateUsers, userController.create);
 router.post("/users/sign_up", userController.signUp);
+router.post("/users/sign_in", validtation.validateUsers, userController.signIn);
 
 // export router
 module.exports = router;
